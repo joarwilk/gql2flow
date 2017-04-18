3d495e2c1e4b1e8bbe87e8bcf18d2ad9334dc862
Gajus Kuizinas <gajus@gajus.com>
2017-04-18 18:14:30 +0100

    fix: syntax error

--- util/interface.js
+++ util/interface.js
@@ -34,10 +34,13 @@ export type GraphQLResponseErrorLocation = {
 
 const generateTypeName = name => `${name}`;
 
-const generateTypeDeclaration = (description, name, possibleTypes) => description ? `/**
-  description: ${description}
-*/` : '';
-export type ${name} = ${possibleTypes};`;
+const generateTypeDeclaration = (description, name, possibleTypes) => {
+  const descriptionComment = description ? `/**
+    description: ${description}
+  */` : '';
+
+  return generateTypeDeclaration + `\nexport type ${name} = ${possibleTypes};`;
+};
 
 const typeNameDeclaration = '__typename: string;\n'
 
